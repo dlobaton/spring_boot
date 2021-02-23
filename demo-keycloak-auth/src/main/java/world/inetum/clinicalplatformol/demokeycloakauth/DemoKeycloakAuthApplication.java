22a6f6752486b9f4a265c714e254dafbd5fe58bb
dlobaton <X03954LO@iecisa.corp>
2021-02-23 13:25:44 +0100

    Spring Boot project Update

---
+++ demo-keycloak-auth/src/main/java/world/inetum/clinicalplatformol/demokeycloakauth/DemoKeycloakAuthApplication.java
@@ -0,0 +1,13 @@
+package world.inetum.clinicalplatformol.demokeycloakauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKeycloakAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKeycloakAuthApplication.class, args);
+	}
+
+}
---
+++ demo-keycloak-auth/src/test/java/world/inetum/clinicalplatformol/demokeycloakauth/DemoKeycloakAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package world.inetum.clinicalplatformol.demokeycloakauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKeycloakAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
